7d78761445b45f9b69e050cd2c05767177db898c
Enrique Lozano <158645923+Phasma2309@users.noreply.github.com>
2024-05-13 22:35:47 -0600

    Add files via upload

---
+++ ADA2 - MetOrdenamiento2/ADA2 - MetOrdenamiento2 - ShellSort.py
@@ -0,0 +1,17 @@
+def shell_sort(arr):
+    n = len(arr)
+    gap = n // 2
+    
+    while gap > 0:
+        for i in range(gap, n):
+            temp = arr[i]
+            j = i
+            while j >= gap and arr[j - gap] > temp:
+                arr[j] = arr[j - gap]
+                j -= gap
+            arr[j] = temp
+        gap //= 2
+    return arr
+
+lista = [23, 12, 1, 8, 34, 54, 2, 3]
+print("Lista ordenada con ShellSort:", shell_sort(lista))
